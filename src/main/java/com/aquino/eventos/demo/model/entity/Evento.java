d7b5d115b80bae8e31fcab418b5773eed6fae962
Leonardo Aquino <aquinoleonardo000@gmail.com>
2026-01-02 12:52:47 -0300

    Conexão com o Banco de Dados

--- src/main/java/com/aquino/eventos/demo/model/entity/Evento.java
+++ src/main/java/com/aquino/eventos/demo/model/entity/Evento.java
@@ -1,9 +1,6 @@
 package com.aquino.eventos.demo.model.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,6 +12,8 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table(name = "evento")
 public class Evento {
 
     @Id
